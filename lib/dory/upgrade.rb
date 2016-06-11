f9f5508909d20bdb37eccf9a8927fd5689e8fe96
Benjamin Porter <bporter@instructure.com>
2016-06-11 04:07:06 +0000

    Add 'upgrade' command for self-updating dory

---
+++ lib/dory/upgrade.rb
@@ -0,0 +1,23 @@
+module Dory
+  module Upgrade
+    def self.new_version
+      res = Dory::Sh.run_command('gem search -q dory')
+      return false unless res.success?
+      newver = /dory\s+\((.*)\)/.match(res.stdout)
+      return false if !newver ||  newver.length != 2
+      newver[1]
+    end
+
+    def self.outdated?(new_version = self.new_version)
+      return Dory::VERSION != new_version
+    end
+
+    def self.install
+      Dory::Sh.run_command('gem install dory')
+    end
+
+    def self.cleanup
+      Dory::Sh.run_command('gem cleanup dory')
+    end
+  end
+end
