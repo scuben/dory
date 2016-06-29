ee9273a656736f255b2820121b7fd8b9f7545bcb
Benjamin Porter <bporter@instructure.com>
2016-06-29 07:34:04 +0000

    Add ability to specify services to up and down
    and also restart.

--- spec/bin/dory_spec.rb
+++ spec/bin/dory_spec.rb
@@ -22,11 +22,31 @@ RSpec.describe DoryBin do
   end
 
   describe 'up' do
-
+    %w[proxy dns resolv].each do |service|
+      it "only starts #{service} when specified by itself" do
+        allow(Dory::Proxy).to receive(:start) { service == 'proxy' }
+        allow(Dory::Dnsmasq).to receive(:start) { service == 'dns' }
+        allow(Dory::Resolv).to receive(:configure) { service == 'resolv' }
+        dory_bin.up(service)
+        expect(Dory::Proxy).send(service == 'proxy' ? :to : :not_to, have_received(:start))
+        expect(Dory::Dnsmasq).send(service == 'dnsmasq' ? :to : :not_to, have_received(:start))
+        expect(Dory::Resolv).send(service == 'resolv' ? :to : :not_to, have_received(:configure))
+      end
+    end
   end
 
   describe 'down' do
-
+    %w[proxy dns resolv].each do |service|
+      it "only stops #{service} when specified by itself" do
+        allow(Dory::Proxy).to receive(:stop) { service == 'proxy' }
+        allow(Dory::Dnsmasq).to receive(:stop) { service == 'dns' }
+        allow(Dory::Resolv).to receive(:clean) { service == 'resolv' }
+        dory_bin.down(service)
+        expect(Dory::Proxy).send(service == 'proxy' ? :to : :not_to, have_received(:stop))
+        expect(Dory::Dnsmasq).send(service == 'dnsmasq' ? :to : :not_to, have_received(:stop))
+        expect(Dory::Resolv).send(service == 'resolv' ? :to : :not_to, have_received(:clean))
+      end
+    end
   end
 
   describe 'version' do
@@ -34,7 +54,23 @@ RSpec.describe DoryBin do
   end
 
   describe 'restart' do
-
+    %w[proxy dns resolv].each do |service|
+      it "only stops #{service} when specified by itself" do
+        allow(Dory::Proxy).to receive(:start) { service == 'proxy' }
+        allow(Dory::Dnsmasq).to receive(:start) { service == 'dns' }
+        allow(Dory::Resolv).to receive(:configure) { service == 'resolv' }
+        allow(Dory::Proxy).to receive(:stop) { service == 'proxy' }
+        allow(Dory::Dnsmasq).to receive(:stop) { service == 'dns' }
+        allow(Dory::Resolv).to receive(:clean) { service == 'resolv' }
+        dory_bin.restart(service)
+        expect(Dory::Proxy).send(service == 'proxy' ? :to : :not_to, have_received(:stop))
+        expect(Dory::Dnsmasq).send(service == 'dnsmasq' ? :to : :not_to, have_received(:stop))
+        expect(Dory::Resolv).send(service == 'resolv' ? :to : :not_to, have_received(:clean))
+        expect(Dory::Proxy).send(service == 'proxy' ? :to : :not_to, have_received(:start))
+        expect(Dory::Dnsmasq).send(service == 'dnsmasq' ? :to : :not_to, have_received(:start))
+        expect(Dory::Resolv).send(service == 'resolv' ? :to : :not_to, have_received(:configure))
+      end
+    end
   end
 
   describe 'status' do
@@ -113,6 +149,59 @@ RSpec.describe DoryBin do
             expect(dory_bin.send("#{service}_enabled?", (settings))).to eq(enabled.values.first)
           end
         end
+      end
+    end
+  end
+
+  describe 'specifcation of services' do
+    context 'sanitization' do
+      it 'defaults to all services if none are specified' do
+        expect(dory_bin.send(:sanitize_services, [])).to eq(dory_bin.send(:valid_services))
+      end
+
+      it 'returns false if an invalid service is present' do
+        expect(dory_bin.send(:sanitize_services, ['badness'])).to be_falsey
+      end
+
+      it 'returns a list of canonical services' do
+        input = %w[nginx-proxy resolve dnsmasq]
+        output = %w[proxy resolv dns]
+        expect(dory_bin.send(:sanitize_services, input)).to match_array(output)
+      end
+    end
+
+    context 'canonicalization of services' do
+      it 'figures out what you mean' do
+        {
+          'proxy' => 'proxy',
+          'nginx' => 'proxy',
+          'nginx_proxy' => 'proxy',
+          'nginx-proxy' => 'proxy',
+          'dns' => 'dns',
+          'dnsmasq' => 'dns',
+          'resolv' => 'resolv',
+          'resolve' => 'resolv'
+        }.each do |input, can|
+          expect(dory_bin.send(:canonical_service, input)).to eq(can)
+        end
+      end
+    end
+
+    it 'validates the service' do
+      {
+        'proxy' => true,
+        'nginx' => true,
+        'nginx_proxy' => true,
+        'nginx-proxy' => true,
+        'dns' => true,
+        'dnsmasq' => true,
+        'resolv' => true,
+        'resolve' => true,
+        'wrong' => false,
+        'hello' => false,
+        'world' => false
+      }.each do |input, valid|
+        expect(dory_bin.send(:valid_service?, input)). to eq(valid)
       end
     end
   end
