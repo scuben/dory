fadd50e9b223051c22748e3a53c7c01d32057607
Benjamin Porter <bporter@instructure.com>
2017-02-14 22:24:33 -0900

    Stub systemctl call on systems without it
    This should fix the unit tests that run on trusty

--- spec/lib/systemd_spec.rb
+++ spec/lib/systemd_spec.rb
@@ -1,107 +1,115 @@
 RSpec.describe Dory::Systemd do
-	let(:cups_disabled_not_running) do
-		return <<~EOF
-			● cups.service - CUPS Scheduler
-				 Loaded: loaded (/usr/lib/systemd/system/cups.service; disabled; vendor preset: enabled)
-				 Active: inactive (dead) since Mon 2017-02-13 15:29:13 AKST; 1min 29s ago
-					 Docs: man:cupsd(8)
-			 Main PID: 3043 (code=exited, status=0/SUCCESS)
-				 Status: "Scheduler is running..."
+  let(:cups_disabled_not_running) do
+    return <<~EOF
+      ● cups.service - CUPS Scheduler
+         Loaded: loaded (/usr/lib/systemd/system/cups.service; disabled; vendor preset: enabled)
+         Active: inactive (dead) since Mon 2017-02-13 15:29:13 AKST; 1min 29s ago
+           Docs: man:cupsd(8)
+       Main PID: 3043 (code=exited, status=0/SUCCESS)
+         Status: "Scheduler is running..."
 
-			Feb 13 05:26:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 06:25:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 07:23:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 08:21:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 09:20:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 10:18:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 11:16:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 15:26:00 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 15:29:13 ben systemd[1]: Stopping CUPS Scheduler...
-			Feb 13 15:29:13 ben systemd[1]: Stopped CUPS Scheduler.
-		EOF
-	end
+      Feb 13 05:26:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 06:25:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 07:23:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 08:21:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 09:20:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 10:18:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 11:16:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 15:26:00 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 15:29:13 ben systemd[1]: Stopping CUPS Scheduler...
+      Feb 13 15:29:13 ben systemd[1]: Stopped CUPS Scheduler.
+    EOF
+  end
 
-	let(:cups_enabled_not_running) do
-		<<~EOF
-			● cups.service - CUPS Scheduler
-				 Loaded: loaded (/usr/lib/systemd/system/cups.service; enabled; vendor preset: enabled)
-				 Active: inactive (dead) since Mon 2017-02-13 15:29:13 AKST; 1s ago
-					 Docs: man:cupsd(8)
-			 Main PID: 3043 (code=exited, status=0/SUCCESS)
-				 Status: "Scheduler is running..."
+  let(:cups_enabled_not_running_retval) do
+      OpenStruct.new(success?: false, exitstatus: 3, stdout: cups_enabled_not_running)
+  end
 
-			Feb 13 05:26:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 06:25:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 07:23:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 08:21:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 09:20:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 10:18:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 11:16:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 15:26:00 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 15:29:13 ben systemd[1]: Stopping CUPS Scheduler...
-			Feb 13 15:29:13 ben systemd[1]: Stopped CUPS Scheduler.
-		EOF
-	end
+  let(:cups_enabled_not_running) do
+    <<~EOF
+      ● cups.service - CUPS Scheduler
+         Loaded: loaded (/usr/lib/systemd/system/cups.service; enabled; vendor preset: enabled)
+         Active: inactive (dead) since Mon 2017-02-13 15:29:13 AKST; 1s ago
+           Docs: man:cupsd(8)
+       Main PID: 3043 (code=exited, status=0/SUCCESS)
+         Status: "Scheduler is running..."
 
-	let(:cups_enabled_and_running) do
-		<<~EOF
-			● cups.service - CUPS Scheduler
-				 Loaded: loaded (/usr/lib/systemd/system/cups.service; enabled; vendor preset: enabled)
-				 Active: active (running) since Fri 2017-02-10 17:38:08 AKST; 2 days ago
-					 Docs: man:cupsd(8)
-			 Main PID: 3043 (cupsd)
-				 Status: "Scheduler is running..."
-					Tasks: 1 (limit: 512)
-				 CGroup: /system.slice/cups.service
-								 └─3043 /usr/sbin/cupsd -l
+      Feb 13 05:26:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 06:25:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 07:23:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 08:21:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 09:20:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 10:18:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 11:16:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 15:26:00 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 15:29:13 ben systemd[1]: Stopping CUPS Scheduler...
+      Feb 13 15:29:13 ben systemd[1]: Stopped CUPS Scheduler.
+    EOF
+  end
 
-			Feb 13 03:30:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 04:28:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 05:26:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 06:25:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 07:23:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 08:21:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 09:20:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 10:18:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 11:16:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 13 15:26:00 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-		EOF
-	end
+  let(:cups_enabled_and_running_retval) do
+      OpenStruct.new(success?: true, stdout: cups_enabled_and_running)
+  end
+
+  let(:cups_enabled_and_running) do
+    <<~EOF
+      ● cups.service - CUPS Scheduler
+         Loaded: loaded (/usr/lib/systemd/system/cups.service; enabled; vendor preset: enabled)
+         Active: active (running) since Fri 2017-02-10 17:38:08 AKST; 2 days ago
+           Docs: man:cupsd(8)
+       Main PID: 3043 (cupsd)
+         Status: "Scheduler is running..."
+          Tasks: 1 (limit: 512)
+         CGroup: /system.slice/cups.service
+                 └─3043 /usr/sbin/cupsd -l
+
+      Feb 13 03:30:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 04:28:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 05:26:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 06:25:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 07:23:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 08:21:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 09:20:18 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 10:18:38 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 11:16:58 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 13 15:26:00 ben cupsd[3043]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+    EOF
+  end
 
   let(:cups_not_installed_retval) do
       OpenStruct.new(success?: false, exitstatus: 3, stdout: cups_not_installed)
   end
 
   let(:cups_not_installed) do
-		<<~EOF
+    <<~EOF
       ● cups.service
          Loaded: not-found (Reason: No such file or directory)
          Active: inactive (dead)
-		EOF
+    EOF
   end
 
   let(:cups_disabled_and_running) do
     <<~EOF
-			● cups.service - CUPS Scheduler
-				 Loaded: loaded (/usr/lib/systemd/system/cups.service; disabled; vendor preset: enabled)
-				 Active: active (running) since Mon 2017-02-13 16:24:20 AKST; 19h ago
-					 Docs: man:cupsd(8)
-			 Main PID: 1201 (cupsd)
-				 Status: "Scheduler is running..."
-					Tasks: 1 (limit: 512)
-				 CGroup: /system.slice/cups.service
-								 └─1201 /usr/sbin/cupsd -l
+      ● cups.service - CUPS Scheduler
+         Loaded: loaded (/usr/lib/systemd/system/cups.service; disabled; vendor preset: enabled)
+         Active: active (running) since Mon 2017-02-13 16:24:20 AKST; 19h ago
+           Docs: man:cupsd(8)
+       Main PID: 1201 (cupsd)
+         Status: "Scheduler is running..."
+          Tasks: 1 (limit: 512)
+         CGroup: /system.slice/cups.service
+                 └─1201 /usr/sbin/cupsd -l
 
-			Feb 14 02:39:21 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 14 03:37:41 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 14 04:36:01 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 14 05:34:21 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 14 06:32:41 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 14 07:31:01 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 14 08:29:21 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 14 09:27:41 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 14 10:26:01 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
-			Feb 14 11:24:21 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 14 02:39:21 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 14 03:37:41 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 14 04:36:01 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 14 05:34:21 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 14 06:32:41 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 14 07:31:01 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 14 08:29:21 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 14 09:27:41 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 14 10:26:01 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
+      Feb 14 11:24:21 ben cupsd[1201]: REQUEST localhost - - "POST / HTTP/1.1" 200 1
     EOF
   end
 
@@ -131,7 +139,7 @@ RSpec.describe Dory::Systemd do
   end
 
   describe '#systemd_service_installed?' do
-		it 'knows cups is installed' do
+    it 'knows cups is installed' do
       [
         cups_enabled_not_running,
         cups_enabled_and_running,
@@ -203,9 +211,24 @@ RSpec.describe Dory::Systemd do
   end
 
   describe '#set_systemd_service' do
+    let(:stub_if_needed) do
+      # on systems without systemd we have to stub this
+      ->(now_running) do
+        unless Dory::Systemd.has_systemd?
+          if (now_running)
+            allow(Dory::Sh).to receive(:run_command) { cups_enabled_and_running_retval }
+          else
+            allow(Dory::Sh).to receive(:run_command) { cups_enabled_not_running_retval }
+          end
+        end
+      end
+    end
+
     it 'puts the service down' do
+      stub_if_needed.call(true)
       Dory::Systemd.set_systemd_service(service: 'cups', up: true)
       expect {
+        stub_if_needed.call(false)
         Dory::Systemd.set_systemd_service(service: 'cups', up: false)
       }.to change {
         Dory::Systemd.systemd_service_running?('cups')
@@ -213,8 +236,10 @@ RSpec.describe Dory::Systemd do
     end
 
     it 'brings the service up' do
+      stub_if_needed.call(false)
       Dory::Systemd.set_systemd_service(service: 'cups', up: false)
       expect {
+        stub_if_needed.call(true)
         Dory::Systemd.set_systemd_service(service: 'cups', up: true)
       }.to change {
         Dory::Systemd.systemd_service_running?('cups')
