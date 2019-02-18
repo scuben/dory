f1b548080a0ae58677cbf47d0c712ff19cd3dc0f
Mads H. Danquah <mads@danquah.dk>
2019-02-18 16:49:44 +0100

    Bump dinghy-http-proxy to 2.5.10
    This release includes a fix of a wrong path that broke tls

--- lib/dory/proxy.rb
+++ lib/dory/proxy.rb
@@ -8,7 +8,7 @@ module Dory
       setting = Dory::Config.settings[:dory][:nginx_proxy][:image]
       return setting if setting
       certs_dir && !certs_dir.empty? \
-        ? 'codekitchen/dinghy-http-proxy:2.5.9' \
+        ? 'codekitchen/dinghy-http-proxy:2.5.10' \
         : 'freedomben/dory-http-proxy:2.5.9.1'
     end
 
